150e36949290937230864ded68b3260608d8b497
patridifusa <p.rufino.a@gmail.com>
2022-11-07 12:11:49 +0000

    Develop push function and change length function

--- js/methods.js
+++ js/methods.js
@@ -1,12 +1,21 @@
 // Get length array
-const getLength = (arr) => {
+export const getLength = (arr) => {
     let counter = 0;
-    arr.forEach(() => {
+    // Count elements in array
+    do {
         counter = counter + 1;
-    });
+    } while (arr[counter] !== undefined);
     return counter;
 };
 
-export const methodsArray = (a) => {
-    return getLength(a);
+export const getPush = (arr, newE) => {
+    // Add elements to the end of array
+    let newIndex = getLength(arr);
+    for (let i = 0; i <= newIndex; i++) {
+        if (i === newIndex) {
+            arr[i] = newE;
+        }
+    }
+    // Return the new length of the array
+    return getLength(arr);
 };
--- js/methods.test.js
+++ js/methods.test.js
@@ -1,15 +1,22 @@
-import { methodsArray } from './methods.js';
+import { getLength, getPush } from './methods.js';
 
-const list = ['orange', 'apple', null, 'orange', -4, 'lemon', undefined];
+const list = ['orange', 'apple', null, 'orange', -4, 'lemon'];
 
 //Primer caso -> Length
 //Le doi el array list y espero que me devuelva el nº total de elementos dentro del array.
 describe('My length function', () => {
-    const expected = 7;
+    const expected = 6;
     test(`Given array ${list}, expected ${expected} length`, () => {
-        expect(methodsArray(list)).toBe(expected);
+        expect(getLength(list)).toBe(expected);
     });
 });
 
 //Segundo caso -> Push
 //Le doi un elemento para el array list y espero que me devuelva el array list con dicho elemento añadido en último lugar.
+describe('My push function', () => {
+    const newElem = 'berries';
+    const expected = 7;
+    test(`Given array, expected ${expected} length`, () => {
+        expect(getPush(list, newElem)).toBe(expected);
+    });
+});
