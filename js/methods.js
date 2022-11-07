49055251b2d0648db711c7a59d0d84a4c4a52718
patridifusa <p.rufino.a@gmail.com>
2022-11-07 09:15:07 +0000

    Develop length function and test

--- js/methods.js
+++ js/methods.js
@@ -1,3 +1,12 @@
-export const methodsArray = (a, b) => {
-    return a + b;
+// Get length array
+const getLength = (arr) => {
+    let counter = 0;
+    arr.forEach(() => {
+        counter = counter + 1;
+    });
+    return counter;
+};
+
+export const methodsArray = (a) => {
+    return getLength(a);
 };
